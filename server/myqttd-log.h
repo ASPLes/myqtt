2f8612a61ab442324a08db078e8dfc8c74adc2bf
Francis Brosnan Blázquez <francis@aspl.es>
2015-04-06 18:55:24 +0000

    myqttd: * [fix] Updates..

--- server/myqttd-log.h
+++ server/myqttd-log.h
@@ -1,27 +1,29 @@
 /* 
  *  MyQtt: A high performance open source MQTT implementation
- *  Copyright (C) 2014 Advanced Software Production Line, S.L.
+ *  Copyright (C) 2015 Advanced Software Production Line, S.L.
  *
  *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2.1
- *  of the License, or (at your option) any later version.
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation version 2.0 of the
+ *  License
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ *  General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this program; if not, write to the Free
- *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- *  02111-1307 USA
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301, USA.
  *  
  *  You may find a copy of the license under this software is released
- *  at COPYING file. This is LGPL software: you are welcome to develop
- *  proprietary applications using this library without any royalty or
- *  fee but returning back any change, improvement or addition in the
- *  form of source code, project image, documentation patches, etc.
+ *  at COPYING file. This is GPL software 
  *
  *  For commercial support on build MQTT enabled solutions contact us:
  *          
@@ -36,7 +38,6 @@
  *         info@aspl.es - http://www.aspl.es/mqtt
  *                        http://www.aspl.es/myqtt
  */
-
 #ifndef __MYQTTD_LOG_H__
 #define __MYQTTD_LOG_H__
 
