0d49d85f7a4e5c4c69730a7ffec33a2f1a6867f7
acinom <acinom@84703cb4-97f3-45fd-98c6-1e4dc641af03>
2015-09-14 14:32:29 +0000

    myqtt: * [fix] Updated message shown when connection closed before finished negotation is   detected to report ip, port and username (client identifier) if finished..

--- lib/myqtt-msg.c
+++ lib/myqtt-msg.c
@@ -645,10 +645,14 @@ MyQttMsg * myqtt_msg_get_next     (MyQttConn * connection)
 			 * the negotiation (just before the initial
 			 * step, but after the second step) */
 			__myqtt_conn_shutdown_and_record_error (
-				connection, MyQttProtocolError, "found connection closed before finishing negotiation, dropping (errno=%d)..",
+				connection, MyQttProtocolError, 
+				"found connection closed before finishing negotiation from=%s:%s username=%s client-id=%s, dropping (errno=%d)..",
+				connection->host_ip, connection->port, 
+				connection->username ? connection->username : "",
+				connection->client_identifier ? connection->client_identifier : "",
 				errno);
 			return NULL;
-		}
+		} /* end if */
 	
 		/* check if we have a non-blocking connection */
 		__myqtt_conn_shutdown_and_record_error (
