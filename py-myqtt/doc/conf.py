bb35747628f3e57dffd937546b89f389edd8e346
Francis Brosnan Blázquez <francis@aspl.es>
2015-10-13 18:06:34 +0000

    myqtt: * [fix] Updated web page to include references..

--- py-myqtt/doc/conf.py
+++ py-myqtt/doc/conf.py
@@ -42,9 +42,9 @@ copyright = '2015, Advanced Software Production Line, S.L.'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.6.3'
+version = '0.6.3.b330'
 # The full version, including alpha/beta/rc tags.
-release = '0.6.3'
+release = '0.6.3.b330'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
